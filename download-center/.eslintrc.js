848a99e67fe50fb8e27eb40edfcc83a0991240c0
Janina Byszewska <jazb@demant.com>
2022-05-27 07:19:15 +0000

    CRA init with eslint proper setup
    This PR is only about the initial installation of the CRA with TS, Eslint, Prettier and Airbnb
    
    commits:
    - CRA with TS
    - initial eslint + prettier settings
    - chore: move all CRA files to one folder
    - refactor: delete unused files
    - chore: add project description
    - feat: adjust eslint rules
    
    Related work items: #245833, #308057

---
+++ download-center/.eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['./'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/prefer-default-export': 0,
+    'react/require-default-props': 0,
+    'react/prop-types': 0,
+    'arrow-body-style': 0,
+  },
+};
